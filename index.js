71e0a9823093dffe4ff0bd234748ba717eb37e03
caleb <calebakpan7@gmail.com>
2023-04-05 15:46:43 +0100

    assigned live NextJS project as cors origin

--- index.js
+++ index.js
@@ -71,7 +71,8 @@ app.use((req, res, next) => {
 })
 
 app.use(cors({
-    origin: "*",
+    // origin: ["http://localhost:3000", "http://192.168.194.235:3000"],
+    origin: 'https://the-network-bice.vercel.app/',
     credentials: true
 }))
 
